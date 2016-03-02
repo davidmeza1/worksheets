fd20bd08af813808d6c0b1c569574bf26ad6b1f3
Lincoln Mullen <lincoln@lincolnmullen.com>
2016-03-02 16:49:45 -0500

    Fix problem with Methodist data going to 1831

--- R/dataprep.R
+++ R/dataprep.R
@@ -5,11 +5,11 @@ library(readr)
 if (!file.exists("data/methodists-uncleaned.csv"))
   download.file("https://docs.google.com/spreadsheets/d/1Ql2-sHAY3BuBmmW3LsX54e3oBUYmqMfLfLsAIsgZoGc/pub?gid=0&single=true&output=csv", "data/methodists-uncleaned.csv")
 
-methodists <- read_csv("data/methodists-uncleaned.csv")
+methodists_raw <- read_csv("data/methodists-uncleaned.csv")
 
-methodists_cleaned <- methodists %>%
+methodists_cleaned <- methodists_raw %>%
   select(-minutes_location, -minutes_date, -notes) %>%
-  filter(minutes_year < 1831,
+  filter(minutes_year <= 1830,
          minutes_year != 1785,
          minutes_year != 1778) %>%
   mutate(members_white   = ifelse(minutes_year >= 1786 & is.na(members_white),
@@ -27,6 +27,7 @@ write_csv(methodists_cleaned, "data/methodists.csv")
 
 va_methodists_wide <- methodists_cleaned %>%
   filter(minutes_year >= 1812,
+         minutes_year <= 1830,
          conference == "Virginia") %>%
   mutate(members_general = members_white + members_colored) %>%
   group_by(minutes_year, conference, district) %>%
